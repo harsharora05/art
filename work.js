050f732f61a05057ae02e6bed4a5e86926e6357a
harsharora05 <hararora12@gmail.com>
2023-07-28 00:47:20 +0530

    structure ready

---
+++ work.js
@@ -0,0 +1,41 @@
+function showart1(){
+    const idname = document.getElementById('art1').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
+
+function showart2(){
+    const idname = document.getElementById('art2').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
+
+function showart3(){
+    const idname = document.getElementById('art3').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
+
+function showart4(){
+    const idname = document.getElementById('art4').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
+
+function showart5(){
+    const idname = document.getElementById('art5').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
+
+function showart6(){
+    const idname = document.getElementById('art6').value;
+    console.log(idname);
+    sessionStorage.setItem("value" , idname);
+    return false;
+}
---
+++ workitem.js
@@ -0,0 +1,55 @@
+window.addEventListener('load' ,() =>{
+    let val = sessionStorage.getItem('value');
+
+    if(val=='art1'){
+
+        document.getElementById("workImg").src = "images/art1.jpg"; 
+
+        document.getElementById('workTitle').innerHTML = "The Starry Night";
+        document.getElementById('workAuthor').innerHTML = " Vincent Van Gogh";
+        document.getElementById('workYear').innerHTML = "1889";
+
+    }
+
+    else if(val == 'art2'){
+        document.getElementById("workImg").src = "images/art2.jpg"; 
+
+        document.getElementById('workTitle').innerHTML = "A Sunday Afternoon on the Island of La Grande Jatte";
+        document.getElementById('workAuthor').innerHTML = "Georges Seurat";
+        document.getElementById('workYear').innerHTML = "1884–1886";
+    }
+
+    else if(val == 'art3'){
+        document.getElementById("workImg").src = "images/art3.jpg"; 
+
+        document.getElementById('workTitle').innerHTML = "The Harvesters ";
+        document.getElementById('workAuthor').innerHTML = "Pieter Bruegel the Elder";
+        document.getElementById('workYear').innerHTML = "1565";
+    }
+
+    else if(val == 'art4'){
+        document.getElementById("workImg").src = "images/art4.jpeg"; 
+
+        document.getElementById('workTitle').innerHTML = "Liberty Leading the People";
+        document.getElementById('workAuthor').innerHTML = "Eugène Delacroix";
+        document.getElementById('workYear').innerHTML = "1830";
+    }
+
+    else if(val == 'art5'){
+        document.getElementById("workImg").src = "images/art5.jpg"; 
+
+        document.getElementById('workTitle').innerHTML = "Claude Monet";
+        document.getElementById('workAuthor').innerHTML = "Impression, Sunrise";
+        document.getElementById('workYear').innerHTML = "1874";
+    }
+
+    else if(val == 'art6'){
+        document.getElementById("workImg").src = "images/art6.jpg"; 
+
+        document.getElementById('workTitle').innerHTML = "The Card Players ";
+        document.getElementById('workAuthor').innerHTML = " Paul Cézanne";
+        document.getElementById('workYear').innerHTML = "1894–95";
+    }
+
+
+})
